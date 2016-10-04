5c6b3059dff7a921c5f2fcb9271eabee2002b2d0
Amir <a.ziyarati@karinaco.com>
2016-10-04 10:50:34 +0330

    some comments

--- app/src/main/java/com/example/amirziarati/mylib/Application.java
+++ app/src/main/java/com/example/amirziarati/mylib/Application.java
@@ -11,6 +11,8 @@ public class Application {
         System.out.println("library was running standlone");
     }
 
+    //this part of code runs in both library run standalone from main or when this library is given to some one else
+    //if the library is something you gonna use in your own project the best practice is to place your component in Android App module Application class
     static {
         System.out.println("android library started");
         component =DaggerMyComponent.builder()
