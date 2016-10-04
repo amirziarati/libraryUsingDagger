a8b75ab9a9d037cf4624a623210659aac8a74add
Amir <a.ziyarati@karinaco.com>
2016-10-04 11:38:14 +0330

    a new comment in Applicaation class

--- app/src/main/java/com/example/amirziarati/mylib/Application.java
+++ app/src/main/java/com/example/amirziarati/mylib/Application.java
@@ -15,9 +15,15 @@ public class Application {
     //if the library is something you gonna use in your own project the best practice is to place your component in Android App module Application class
     static {
         System.out.println("android library started");
-        component =DaggerMyComponent.builder()
-                .myModule(new MyModule())
-                .build();
+        component =DaggerMyComponent.builder().myModule(new MyModule()).build();
+
+//        you only need to put   ".myModule(new MyModule())" for modules
+//        which you need to send them a arguments in their constructor
+//        e.g.         DaggerMyComponent.builder().myModule(new MyModule(String someArgument)).build();
+
+//        so the below line would also work instead of above
+//        component =DaggerMyComponent.builder().build();
+
         MainClass obj=new MainClass();
         System.out.println("android library ended");    }
 }
